1cead7c7537c3a2c1e37c6d88380c7c094ede058
baljinder <baljindermaan15@gmail.com>
2024-10-03 18:31:00 -0400

    blog post fix

--- RepoPattern/Models/Blog.cs
+++ RepoPattern/Models/Blog.cs
@@ -2,10 +2,21 @@ namespace Repo_Pattern.RepoPattern;
 
     public class Blog {
         
-        public string Description {get; set;}
+        public string? Description {get; set;}
         public string Name {get; set;}
         public string URL {get; set;}
 
-        public List<BlogPost> BlogPosts{get; set;}
+        public List<BlogPost>? BlogPosts{get; set;}
+
+        protected Blog() {}
+
+        public Blog(string url, string name){
+            this.URL = url;
+            this.Name = name;
+        }
+        public Blog(string url, string name, string description) : this(url, name){
+            
+            this.Description = description;
+        }
 
     }
